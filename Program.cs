ce2e1e45ca128b059ddc26fd632d3bdd043dd06a
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-12-19 22:11:59 +0900

    Add while(ture)

--- Program.cs
+++ Program.cs
@@ -9,68 +9,66 @@ using System.Threading;
 
 namespace ConsoleApp1
 {
-    class Program
+  class Program
+  {
+    static ManualResetEvent allDone;
+    static void Main()
     {
-        static void Main()
-        {
-            string ipString = "127.0.0.1";
+      allDone = new ManualResetEvent(false);
+      string ipString = "127.0.0.1";
 
-            System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
+      System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
 
-            int port = 2001;
-            IPEndPoint localEndPoint = new IPEndPoint(ipAdd, port); 
+      int port = 2001;
+      IPEndPoint localEndPoint = new IPEndPoint(ipAdd, port); 
 
-            Socket listener = new Socket(ipAdd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+      Socket listener = new Socket(ipAdd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            listener.Bind(localEndPoint);
-            listener.Listen(100);
-            StartAccept(listener);
-            System.Threading.Thread.Sleep(2000);
-        }
-
-        //クライアントの接続待ちスタート
-        private static void StartAccept(System.Net.Sockets.Socket server)
-        {
-            //接続要求待機を開始する
-            Console.WriteLine("10");
-            System.Threading.Thread.Sleep(2000);
-            server.BeginAccept(
-                new System.AsyncCallback(AcceptCallback), server);
-            Console.WriteLine("11");
-            System.Threading.Thread.Sleep(2000);
-        }
-
-        //BeginAcceptのコールバック
-        private static void AcceptCallback(System.IAsyncResult ar)
-        {
-            Console.WriteLine("12");
-            //サーバーSocketの取得
-
-            System.Net.Sockets.Socket server =
-                (System.Net.Sockets.Socket)ar.AsyncState;
-
-            //接続要求を受け入れる
-            System.Net.Sockets.Socket client = null;
-            try
-            {
-                //クライアントSocketの取得
-                client = server.EndAccept(ar);
-            }
-            catch
-            {
-                System.Console.WriteLine("閉じました。");
-                return;
-            }
-
-            //クライアントが接続した時の処理をここに書く
-            //ここでは文字列を送信して、すぐに閉じている
-            client.Send(System.Text.Encoding.UTF8.GetBytes("こんにちは。"));
-            client.Shutdown(System.Net.Sockets.SocketShutdown.Both);
-            client.Close();
-
-            //接続要求待機を再開する
-            server.BeginAccept(
-                new System.AsyncCallback(AcceptCallback), server);
-        }
+      listener.Bind(localEndPoint);
+      listener.Listen(100);
+        
+      while(true)
+      {
+        allDone.Reset();
+        StartAccept(listener);
+        allDone.WaitOne();
+      }
     }
+
+    //クライアントの接続待ちスタート
+    private static void StartAccept(System.Net.Sockets.Socket server)
+    {
+      //接続要求待機を開始する
+      server.BeginAccept(new System.AsyncCallback(AcceptCallback), server);
+    }
+
+    //BeginAcceptのコールバック
+    private static void AcceptCallback(System.IAsyncResult ar)
+    {
+    //サーバーSocketの取得
+    System.Net.Sockets.Socket server = (System.Net.Sockets.Socket)ar.AsyncState;
+
+    //接続要求を受け入れる
+      System.Net.Sockets.Socket client = null;
+      try
+      {
+        //クライアントSocketの取得
+        client = server.EndAccept(ar);
+      }
+      catch
+      {
+        System.Console.WriteLine("閉じました。");
+        return;
+      }
+
+      //クライアントが接続した時の処理をここに書く
+      //ここでは文字列を送信して、すぐに閉じている
+      client.Send(System.Text.Encoding.UTF8.GetBytes("こんにちは。"));
+      client.Shutdown(System.Net.Sockets.SocketShutdown.Both);
+      client.Close();
+
+      //接続要求待機を再開する
+      server.BeginAccept(new System.AsyncCallback(AcceptCallback), server);
+    }
+  }
 }
--- history.md
+++ history.md
@@ -27,7 +27,16 @@ System.Net.Sockets.SocketException (0x80004005): The requested address is not va
   at System.Net.Sockets.Socket.Bind (System.Net.EndPoint localEP) [0x00043] in <dfffed4750cb49c8ab16f18fc177a88e>:0 
   at ConsoleApp1.Program.Main () [0x00072] in <712b8abf7d1b4ceca17ac9c69535e7df>:0 
 ```
+### ipアドレスを127.0.0.1に変更
 
-### 動いているサンプルプログラムと比較する
+### BeginAcceptを繰り返し実行
+
+BeginAccept(listener)は非同期のため、接続要求がなくても値を返してしまう。そこで、ManualResetEventを組み合わせて繰り返し実行し、この行の値が返ってもプログラムを終了させず、繰り返し実行するようにした。
+
+
+参考: [非同期サーバー ソケットの使用](https://docs.microsoft.com/ja-jp/dotnet/framework/network-programming/using-an-asynchronous-server-socket)
+参考: [C# 非同期ソケット通信で簡易サーバーを作成 - エクセレンス★ブログ](https://www.excellence-blog.com/2018/06/08/c-%E9%9D%9E%E5%90%8C%E6%9C%9F%E3%82%BD%E3%82%B1%E3%83%83%E3%83%88%E9%80%9A%E4%BF%A1%E3%81%A7%E7%B0%A1%E6%98%93%E3%82%B5%E3%83%BC%E3%83%90%E3%83%BC%E3%82%92%E4%BD%9C%E6%88%90/)
+
+### (やってない)動いているサンプルプログラムと比較する
 
 参考: [#Cでsocket通信をしてみる。ついでにnetstatでlistenしているポートを調べる。](http://karoten512.hatenablog.com/entry/2018/03/21/234156)
